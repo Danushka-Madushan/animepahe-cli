0544a68c665633e425ec279573a463912b76046e
Danushka-Madushan <danushkamadushan128@gmail.com>
2025-11-15 20:47:00 +0530

    update main.cpp

--- main.cpp
+++ main.cpp
@@ -48,7 +48,7 @@ int main(int argc, char *argv[])
     ("h,help", "Print usage");
 
     /* version tag */
-    const std::string VERSION = "v0.2.2-beta";
+    const std::string VERSION = "v0.2.3-beta";
 
     try
     {
