23c658ad524870e44a634182665b953abbec612a
Danushka-Madushan <danushkamadushan128@gmail.com>
2026-01-17 14:40:58 +0530

    update from 0.2.5-beta to 0.2.6-beta

--- main.cpp
+++ main.cpp
@@ -48,7 +48,7 @@ int main(int argc, char *argv[])
     ("h,help", "Print usage");
 
     /* version tag */
-    const std::string VERSION = "v0.2.5-beta";
+    const std::string VERSION = "v0.2.6-beta";
 
     try
     {
